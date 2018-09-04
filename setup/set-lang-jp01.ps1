feab97951192f63ffac1d9914e19a41d15d5bf1d
h_hoshino <h_hoshino@K-MAC010noMacBook-Pro.local>
2018-09-04 09:57:26 +0900

    add:setupTools

---
+++ setup/set-lang-jp01.ps1
@@ -0,0 +1,10 @@
+$LpUrl = "http://download.windowsupdate.com/c/msdownload/update/software/updt/2016/09/"
+$LpFile = "lp_9a666295ebc1052c4c5ffbfa18368dfddebcd69a.cab"
+$LpTemp = "C:\LpTemp.cab"
+Set-WinUserLanguageList -LanguageList ja-JP,en-US -Force
+Start-BitsTransfer -Source $LpUrl$LpFile -Destination $LpTemp -Priority High
+Add-WindowsPackage -PackagePath $LpTemp -Online
+Set-WinDefaultInputMethodOverride -InputTip "0411:00000411"
+Set-WinLanguageBarOption -UseLegacySwitchMode -UseLegacyLanguageBar
+Remove-Item $LpTemp -Force
+Restart-Computer
